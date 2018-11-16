c2a4e37e14d37f69274f58c51ed6f8d65680a07f
小雨制造 <xy609284278@126.com>
2018-11-16 16:08:18 +0800

    增加webSocket对接

---
+++ RegistryLibrary/RegistryLibrary/ImplementsClass/WebSocketData.cs
@@ -0,0 +1,29 @@
+﻿namespace RegistryLibrary.ImplementsClass
+{
+    /// <summary>
+    /// WebSocket数据通信实体
+    /// </summary>
+    public class WebSocketData
+    {
+        /// <summary>
+        /// 本次会话的唯一标识
+        /// 该值由前台自动生成, 后台仅作返回, 不进行其他逻辑处理
+        /// </summary>
+        public string Seed { get; set; }
+
+        /// <summary>
+        /// 访问类型
+        /// </summary>
+        public WebSocketDataEnum Type { get; set; }
+
+        /// <summary>
+        /// 发送频道
+        /// </summary>
+        public string Channel { get; set; }
+
+        /// <summary>
+        /// 传递数据
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
---
+++ RegistryLibrary/RegistryLibrary/ImplementsClass/WebSocketDataEnum.cs
@@ -0,0 +1,23 @@
+﻿namespace RegistryLibrary.ImplementsClass
+{
+    /// <summary>
+    /// 传输类型枚举
+    /// </summary>
+    public enum WebSocketDataEnum
+    {
+        /// <summary>
+        /// 提交数据并等待回复
+        /// </summary>
+        Submit = 0,
+
+        /// <summary>
+        /// 发布事件
+        /// </summary>
+        Publish = 1,
+
+        /// <summary>
+        /// 订阅频道
+        /// </summary>
+        Subscribe = 2,
+    }
+}
